7c5ab426bc97c2320990049eec79449bfe7b6a01
Emilia <emiliama2006@gmail.com>
2026-09-09 17:01:51 -0400

    mobile pan test fix + page2 more

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -16,14 +16,14 @@ export default function AboutMe() {
                         <img className="aboutme-placed-item aboutme-portrait aboutme-page1-portrait-placement" alt="a"
                              src="/assets/browser/content/about-me/me-2.webp"/>
                         <img className="aboutme-placed-item aboutme-tape aboutme-page1-tape-placement" alt="a"
-                             src="/assets/browser/content/about-me/tape.png"/>
+                             src="/assets/browser/content/about-me/drawings/tape.png"/>
                         <img className="aboutme-placed-item aboutme-plant aboutme-page1-plant-placement" alt="a"
-                             src="/assets/browser/content/about-me/plant.png"/>
+                             src="/assets/browser/content/about-me/drawings/plant.png"/>
                         <div className="aboutme-text">welcome to my portfolio!</div>
                         <div className="aboutme-text">i'm a 3rd year computer science</div>
                         <div className="aboutme-text">major at the university of toronto</div>
                         <img className="aboutme-placed-item page1-longhighlight-1 aboutme-highlight" alt="a"
-                             src="/assets/browser/content/about-me/long-highlight.png"/>
+                             src="/assets/browser/content/about-me/drawings/long-highlight.png"/>
                         <div className="aboutme-text">☆</div>
                         <div className="aboutme-text">
                             <span>currently working on </span>
@@ -33,13 +33,13 @@ export default function AboutMe() {
                         </div>
                         <div className="aboutme-text"> and my software dev internship</div>
                         <img className="aboutme-placed-item page1-longhighlight-2 aboutme-highlight" alt="a"
-                             src="/assets/browser/content/about-me/long-highlight.png"/>
+                             src="/assets/browser/content/about-me/drawings/long-highlight.png"/>
                         <div className="aboutme-text">at the ontario government</div>
                         <div className="aboutme-text">☆</div>
                         <div className="aboutme-text">i ♥ making things that feel fun & meaningful to me</div>
                         <div className="aboutme-text"></div>
                         <img className="aboutme-placed-item page1-underline-1" alt="a"
-                             src="/assets/browser/content/about-me/underline.png"/>
+                             src="/assets/browser/content/about-me/drawings/underline.png"/>
                         <div className="aboutme-text">
                             <span> and nothing beats seeing it all come together</span>
                             <span className="aboutme-text-withstar">‎ ☆</span>
@@ -55,22 +55,33 @@ export default function AboutMe() {
                         <div className="aboutme-title-2"> bits of my life</div>
                         <div className="aboutme-text">Outside of coding, I’m usually lifting,</div>
                         <div className="aboutme-page2-gif-group">
-                            <img className="aboutme-placed-item aboutme-page2-gif-placement" alt="a"
+                            <img className="aboutme-placed-item aboutme-page2-image-relative-placement" alt="a"
                                  src="/assets/browser/content/about-me/cat-gym.gif"/>
                             <img className="aboutme-placed-item aboutme-tape aboutme-page2-tape-placement" alt="a"
-                                 src="/assets/browser/content/about-me/big-tape.png"/>
+                                 src="/assets/browser/content/about-me/drawings/big-tape.png"/>
+                            <img className="aboutme-placed-item aboutme-tape aboutme-page2-tape-placement-2" alt="a"
+                                 src="/assets/browser/content/about-me/drawings/tape.png"/>
                             <div className="aboutme-page2-caption-placement aboutme-placed-item">me btw</div>
-                            <img className="aboutme-placed-item aboutme-arrow-placement-1" alt="a" src="/assets/browser/content/about-me/white-arrow.png"/>
+                            <img className="aboutme-placed-item aboutme-arrow-placement-1" alt="a"
+                                 src="/assets/browser/content/about-me/drawings/white-arrow.png"/>
                         </div>
+                        <div className="aboutme-page2-image2group-placement">
+                            <img className="aboutme-placed-item aboutme-page2-image-relative-placement" alt="a"
+                                 src="/assets/browser/content/about-me/food.jpg"/>
+                            <img className="aboutme-placed-item aboutme-tape aboutme-page2-tape-placement-3" alt="a"
+                                 src="/assets/browser/content/about-me/drawings/small-tape.png"/>
+                            <img className="aboutme-placed-item aboutme-tape aboutme-page2-tape-placement-4" alt="a"
+                                 src="/assets/browser/content/about-me/drawings/small-tape.png"/>
+                        </div>
+                        <div className="aboutme-text aboutme-page2-food-line">looking for good food,</div>
                         {/*<div className="aboutme-text">i love 2d men</div>*/}
                     </div>
                 </div>
             </div>
-            {/* Outside of coding, I’m usually lifting, wandering somewhere new, or stopping to take a photo because the
-            light looked nice. My camera roll is mostly cats, good food, outfits, and little scenes I didn’t want to forget.*/}
+            {/* Outside of coding, I’m usually lifting, wandering around for good food, or stopping to take a photo because the
+            light looked nice. My camera roll is mostly cats, outfits, and little scenes I didn’t want to forget.*/}
         </div>
         <div className="aboutme-scroll-space"/>
     </div>
 }
-
 

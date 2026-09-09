ef0a4763bc0fc3fefdce1c529067162a808686b5
Emilia <emiliama2006@gmail.com>
2026-09-08 21:34:26 -0400

    more fonts, make notebook-anchor so all elements inside scale and stick regardless of browser dimensions

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,7 +2,13 @@ export default function AboutMe() {
 
     return <div className="outer-aboutme-container">
         <img className="aboutme-behindnotebook" alt="a" src="/assets/browser/content/about-me/desk.jpg"/>
-        <img className="aboutme-background-img" alt="a" src="/assets/browser/content/about-me/Notebook.png"/>
+        <div className="notebook-anchor">
+            <img className="aboutme-background-img" alt="a" src="/assets/browser/content/about-me/Notebook.png"/>
+            <div className="aboutme-page1-container">
+                <div className="aboutme-title"> hello! :)</div>
+                <div className="aboutme-text"></div>
+            </div>
+        </div>
     </div>
 }
 

075f731990f0182f0a141108aa5b6d6b50c43da5
Emilia <emiliama2006@gmail.com>
2026-09-09 09:09:49 -0400

    fixing line spacing

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,14 +3,14 @@ import './AboutMe.css'
 export default function AboutMe() {
 
     return <div className="outer-aboutme-container">
-        <img className="aboutme-behindnotebook" alt="a" src="/assets/browser/content/about-me/desk.jpg"/>
+        <img className="aboutme-behindnotebook" alt="a" src="/assets/browser/content/about-me/desk.webp"/>
         <div className="notebook-anchor">
-            <img className="aboutme-background-img" alt="a" src="/assets/browser/content/about-me/Notebook.png"/>
+            <img className="aboutme-background-img" alt="a" src="/assets/browser/content/about-me/Notebook.webp"/>
             <div className="aboutme-page1-container">
                 <div className="aboutme-title-1"> hello! :)</div>
                 <div className="aboutme-text-withstar">i'm emilia ☆</div>
                 <img className="aboutme-placed-item aboutme-portrait aboutme-page1-portrait-placement" alt="a"
-                     src="/assets/browser/content/about-me/me 2.jpg"/>
+                     src="/assets/browser/content/about-me/me-2.webp"/>
                 <img className="aboutme-placed-item aboutme-tape aboutme-page1-tape-placement" alt="a"
                      src="/assets/browser/content/about-me/tape.png"/>
                 <img className="aboutme-placed-item aboutme-plant aboutme-page1-plant-placement" alt="a"

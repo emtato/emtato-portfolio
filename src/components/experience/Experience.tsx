c2220ddb6cb4a368a9cbed33e32d499f7df5f190
Emilia <emiliama2006@gmail.com>
2026-09-10 17:00:54 -0400

    update experience: icons!! (work badhge :3)

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -5,12 +5,27 @@ export default function Experience() {
     return (
 
         <div className="experience-container">
-            <div className="work-experience-container">
+            <section className="work-experience-container">
+                <header className="section-header">
+                    <img className="section-number" alt="a" src="/assets/browser/content/experience/work.png"/>
+                    <div className="section-uppertext">My Career so far</div>
+                    <h2 className="section-title">Experience</h2>
+                    <div className="section-accent"/>
+                </header>
+                <div className="work-experience-timeline">
 
-            </div>
-            <div className="projects-container">
+                </div>
+            </section>
+            <section className="projects-container">
+                <header className="section-header">
+                    <div className="section-uppertext">Selected projects</div>
+                    <h2 className="section-title">Things I’ve Built</h2>
+                    <div className="section-accent"/>
+                </header>
+                <div className="projects-grid">
 
-            </div>
+                </div>
+            </section>
         </div>
     )
 }

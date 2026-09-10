87700be5cbb0907e201716743a2c9c5c9e72edcf
Emilia <emiliama2006@gmail.com>
2026-09-10 17:48:49 -0400

    updated asset styles, placing image for exeprience

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -7,7 +7,7 @@ export default function Experience() {
         <div className="experience-container">
             <section className="work-experience-container">
                 <header className="section-header">
-                    <img className="section-number" alt="a" src="/assets/browser/content/experience/work.png"/>
+                    <img className="section-image" alt="a" src="/assets/browser/content/experience/work.png"/>
                     <div className="section-uppertext">My Career so far</div>
                     <h2 className="section-title">Experience</h2>
                     <div className="section-accent"/>
@@ -18,6 +18,7 @@ export default function Experience() {
             </section>
             <section className="projects-container">
                 <header className="section-header">
+                    <img className="section-image" alt="a" src="/assets/browser/content/experience/code.png"/>
                     <div className="section-uppertext">Selected projects</div>
                     <h2 className="section-title">Things I’ve Built</h2>
                     <div className="section-accent"/>

ef280e691827193c1dcb14eaaccc3cb16a964703
Emilia <emiliama2006@gmail.com>
2026-09-12 14:08:44 -0400

    decorative items progress

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -24,10 +24,13 @@ export default function Experience() {
                     <h2 className="section-title">Experience</h2>
                     <div className="section-accent"/>
                 </header>
+
                 <div className="work-experience-timeline">
                     <div className="work-experience-timeline-line-container">
                         <div className="timeline-segment segment-small season-fall"/>
                         <div className="timeline-insertion-point">
+                            <img className="timeline-art TLA-fall2026" alt="a"
+                                 src="/assets/browser/content/experience/fall-1.png"/>
                             <TimelineCard
                                 title="Backend developer / Junior Technical Analyst"
                                 organization="Ontario Public Service"
@@ -44,12 +47,46 @@ export default function Experience() {
                         <div className="timeline-segment segment-small season-fall"/>
                         <div className="timeline-segment segment-small season-summer"/>
                         <div className="timeline-segment segment-small season-spring"/>
+                        <div className="timeline-segment segment-small season-winter"/>
+                        <div className="timeline-insertion-point">
+                            <img className="timeline-art TLA-winter2026" alt="a"
+                                 src="/assets/browser/content/experience/winter.png"/>
+                            <TimelineCard
+                                title="hackathon"
+                                organization="Ontario asdasdada Service"
+                                context="Digital adasdas Branch · 4-8 month internship"
+                                description="Working on adasdasdas Java backend behind Ontario’s driver and vehicle services, with a focus on system modernization, testing, and core business logic."
+                                tags={["a"]}
+                                dateRange="jan"
+                                season="winter"
+                                highlights={["AHDIAHIDA", "ANJKANKJAA"]}
+                                mini={true}
+                            />
+                            <div className="timeline-marker season-winter"/>
+                        </div>
                         <div className="timeline-segment segment-xs season-winter"/>
                         <div className="timeline-dot season-winter"/>
                         <div className="timeline-year-text TYT-right">2026</div>
-                        <div className="timeline-segment segment-xs season-winter"/>
+                        <div className="timeline-segment segment-small season-winter"/>
                         <div className="timeline-segment segment-small season-fall"/>
-                        <div className="timeline-segment segment-small season-summer"/>
+                        <div className="timeline-segment segment-xs season-summer"/>
+                        <div className="timeline-insertion-point">
+                            <img className="timeline-art TLA-winter2026" alt="a"
+                                 src="/assets/browser/content/experience/summer.png"/>
+                            <TimelineCard
+                                title="deans list scholar"
+                                organization="Ontario asdasdada Service"
+                                context="Digital adasdas Branch · 4-8 month internship"
+                                description="Working on adasdasdas Java backend behind Ontario’s driver and vehicle services, with a focus on system modernization, testing, and core business logic."
+                                tags={["a"]}
+                                dateRange="summer idk"
+                                season="summer"
+                                highlights={["AHDIAHIDA", "ANJKANKJAA"]}
+                                mini={true}
+                            />
+                            <div className="timeline-marker season-summer"/>
+                        </div>
+                        <div className="timeline-segment segment-xs season-summer"/>
                         <div className="timeline-segment segment-small season-spring"/>
                         <div className="timeline-segment segment-xs season-winter"/>
                         <div className="timeline-dot season-winter"/>
@@ -98,17 +135,16 @@ export default function Experience() {
                                 context=""
                                 description="A"
                                 tags={["UOP"]}
-                                dateRange="Fall 2023 - Fall 2024"
+                                dateRange="Fall 2022 - Fall 2023"
                                 season="fall"
                                 mini={true}
                             />
                             <div className="timeline-marker season-fall"/>
                         </div>
-                        <div className="timeline-segment segment-small season-fall"/>
-                        <div className="timeline-segment segment-small season-summer"/>
                     </div>
                 </div>
             </section>
+            {/*projects*/}
             <section className="projects-container">
                 <header className="section-header">
                     <img className="section-image" alt="a" src="/assets/browser/content/experience/code.png"/>

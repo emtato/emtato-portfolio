97c30f04bd6c1ab2068e3ef3d37f86cd3c4967cf
Emilia <emiliama2006@gmail.com>
2026-09-11 20:38:48 -0400

    change segment-container width to transfer 1.2% of width to segment complete groundwork for all reusable pieces for timeline

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -16,12 +16,13 @@ export default function Experience() {
                 <div className="work-experience-timeline">
                     <div className="work-experience-timeline-line-container">
                         <div className="timeline-segment season-spring"/>
-                        <div className="timeline-segment segment-long season-fall"/>
-                        <div className="timeline-dot season-fall"/>
-                        <div className="timeline-insertion-point">
+                         <div className="timeline-insertion-point">
                             <TimelineCard></TimelineCard>
                             <div className="timeline-marker season-fall"/>
                         </div>
+                        <div className="timeline-segment segment-long season-fall"/>
+                        <div className="timeline-dot season-fall"/>
+
                         <div className="timeline-year-text TYT-right">2204</div>
                     </div>
                 </div>

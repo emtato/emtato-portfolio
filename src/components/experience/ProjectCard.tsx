7a738aaf8cf49fcf4543c81208770bfd4bc0eacd
Emilia <emiliama2006@gmail.com>
2026-09-13 17:18:00 -0400

    starting detailed project view

--- src/components/experience/ProjectCard.tsx
+++ src/components/experience/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 interface ProjectProps {
     title: string
@@ -19,35 +19,59 @@ export default function ProjectCard({
                                     }: ProjectProps) {
     const [isHovered, setIsHovered] = useState(false) /* TODO: hover effects*/
     const [isOpen, setisOpen] = useState(false)
+    const cardRef = useRef<HTMLElement>(null)
 
-    return <article className="project-card">
-        <div className="project-card-image-wrapper">
-            {cardImg && <img className="project-card-image" alt='a' src={cardImg}/>}
-        </div>
-        <div className="project-card-text-wrapper">
-            <div className="project-card-heading">
-                <div className="project-card-title-group">
-                    <h3 className="project-card-title">{title}</h3>
-                    <div className="project-card-links">
-                        {projectLink && <a className="project-card-link-button" href={projectLink} target="_blank">
-                            <img className="project-card-link-icon" alt="a" src="/assets/system/link-icon-hover.png"/>
-                        </a>}
-                        {github && <a className="project-card-link-button-gh" href={github} target="_blank">
-                            <img className="project-card-github-icon" alt=""
-                                 src="/assets/browser/content/experience/github.png"/>
-                        </a>}
-                    </div>
-                </div>
+    useEffect(() => {
+        function handleOutsideClick(event: MouseEvent) {
+            if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+                setisOpen(false)
+            }
+        }
 
-                {timeInfo && <div className="project-card-timeInfo">{timeInfo}</div>}
+        document.addEventListener("mousedown", handleOutsideClick)
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick)
+        }
+    }, [])
+
+    return <>
+        <article className="project-card" onClick={() => setisOpen(true)} ref={cardRef}>
+            <div className="project-card-image-wrapper">
+                {cardImg && <img className="project-card-image" alt='a' src={cardImg}/>}
             </div>
-            <p className="project-card-shortDesc">{shortDesc}</p>
+            <div className="project-card-text-wrapper">
+                <div className="project-card-heading">
+                    <div className="project-card-title-group">
+                        <h3 className="project-card-title">{title}</h3>
+                        <div className="project-card-links">
+                            {projectLink && <a className="project-card-link-button" href={projectLink} target="_blank"
+                                               onClick={(event) => event.stopPropagation()}>
+                                <img className="project-card-link-icon" alt="a"
+                                     src="/assets/system/link-icon-hover.png"/>
+                            </a>}
+                            {github && <a className="project-card-link-button-gh" href={github} target="_blank"
+                                          onClick={(event) => event.stopPropagation()}>
+                                <img className="project-card-github-icon" alt=""
+                                     src="/assets/browser/content/experience/github.png"/>
+                            </a>}
+                        </div>
+                    </div>
 
-            {stack.length > 0 && <div className="project-card-stack-wrapper">
-                {stack.map((technology) =>
-                    <span className="project-card-stack-tag" key={technology}>{technology}</span>
-                )}
-            </div>}
-        </div>
-    </article>
+                    {timeInfo && <div className="project-card-timeInfo">{timeInfo}</div>}
+                </div>
+                <p className="project-card-shortDesc">{shortDesc}</p>
+
+                {stack.length > 0 && <div className="project-card-stack-wrapper">
+                    {stack.map((technology) =>
+                        <span className="project-card-stack-tag" key={technology}>{technology}</span>
+                    )}
+                </div>}
+            </div>
+
+        </article>
+        {isOpen && <div className="opened-project-card">
+            detailed project view coming soon!
+            <div className="opened-card-x">×</div>
+        </div>}
+    </>
 }

a6e84a8142d034b2bd04203720c55d40e7506abe
Emilia <emiliama2006@gmail.com>
2026-09-18 13:54:23 -0400

    added tempo desktop

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -9,11 +9,18 @@ export default function Desktop({openBrowserCallback}: DesktopProps) {
     return <div className="desktop">
         <div className="apps-list">
             <div className="app-wrapper">
-                <div className="dsk-pp-button" onClick={openBrowserCallback}>
+                <div className="dsk-app-button" onClick={openBrowserCallback}>
                     <img className="dsk-app-icon" alt="a" src="/assets/app-icons/potato-browser.png"/>
                     <div className="dsk-app-label">potato browser</div>
                 </div>
             </div>
+            <div className="app-wrapper">
+                <a className="dsk-app-button" href="https://tempo.ems.lol" target="_blank">
+                    <img className="dsk-app-icon" alt="a" src="/assets/app-icons/Calendar.png"/>
+                    <div className="dsk-app-label">tempo</div>
+                </a>
+            </div>
         </div>
+        {/* TODO: photo gallery for more of my photos*/}
     </div>
 }

f2398257fb58026c910708e2774e35e083fcf541
Emilia <emiliama2006@gmail.com>
2026-09-18 22:12:23 -0400

    start contact card

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,7 +3,18 @@ import './Contact.css'
 export default function Contact() {
     return (
         <div className="contact-container">
-            helooo
+            <div className="contact-ratio-box">
+                <img className="contact-container-background" alt="a"
+                     src="/assets/browser/content/contact/contact-background.png"/>
+                <div className="contact-email-section">
+                    <div className="contact-emailto-text">
+                        To: emilia.ma@mail.utoronto.ca
+                    </div>
+                    <div className="contact-emailfrom-text">
+                        From: meow
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

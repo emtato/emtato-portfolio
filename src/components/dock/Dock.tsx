136f892d11bef668b8d075f4dbb97d6216fb9752
Emilia <emiliama2006@gmail.com>
2026-09-24 11:28:29 -0400

    more icons, click effects!

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -1,5 +1,6 @@
 import './dock.css'
 import DockApp from './dockApp/dockapp.tsx'
+import {useEffect, useState} from "react";
 
 interface DockProps {
     openBrowserCallback: () => void
@@ -8,11 +9,23 @@ interface DockProps {
 }
 
 export default function Dock({openBrowserCallback, openContact, openTempo}: DockProps) {
+    const [copied, setCopied] = useState(false)
+    useEffect(() => {
+        if (!copied) return
+
+        const copiedTimer = window.setTimeout(() => setCopied(false), 1500)
+        return () => window.clearTimeout(copiedTimer)
+    }, [copied])
 
     function openBrowser() {
         openBrowserCallback()
     }
 
+
+    async function copyDiscord() {
+        await navigator.clipboard.writeText("@.emtato");
+        setCopied(true)
+    }
 
     return <div className="dock">
         <div className="dock-background">
@@ -25,12 +38,18 @@ export default function Dock({openBrowserCallback, openContact, openTempo}: Dock
             <DockApp src="/assets/app-icons/potato-browser.png" onClick={openBrowser} hoverText="potato browser"/>
             {/*TODO: onhover, show app name and outline icon white or make it highlighted idk*/}
             <DockApp src="/assets/app-icons/mail.png" onClick={openContact} hoverText="contact me"/>
-            <DockApp src="/assets/app-icons/linkedin.png" href="https://linkedin.com/in/emilia-ma" hoverText="linkedin"/>
+            <DockApp src="/assets/app-icons/linkedin.png" href="https://linkedin.com/in/emilia-ma"
+                     hoverText="linkedin"/>
             <DockApp src="/assets/app-icons/calendar.png" onClick={openTempo} hoverText="tempo"/>
-            <DockApp src="/assets/app-icons/uwucord.png" hoverText="uwucord"/>
-            <DockApp src="/assets/app-icons/map.png" hoverText="map"/>
-            <DockApp src="/assets/app-icons/photos.png" rotate={true} hoverText="photos"/>
-            <DockApp src="/assets/app-icons/contacts.png" hoverText="contacts"/>
+            <DockApp src="/assets/app-icons/uwucord.png" hoverText="uwucord" onClick={copyDiscord}/>
+            {copied && <span className="dock-app-copied">copied!</span>}
+
+            <DockApp src="/assets/app-icons/map.png" hoverText="map (coming soon)"/>
+            <DockApp src="/assets/app-icons/photos.png" rotate={true} hoverText="photos (coming soon)"/>
+            <DockApp src="/assets/app-icons/contacts.png" hoverText="contacts" onClick={openContact}/>
+            <DockApp src="/assets/app-icons/music.png" hoverText="peak" href="https://www.youtube.com/watch?v=kgAVoL49H3M"/>
+            {/*https://www.youtube.com/watch?v=83C3TZ4Zm_o LEMONADE*/}
+            <DockApp src="/assets/app-icons/trash.png" hoverText="trash"/>
         </div>
     </div>
 }
